01612e4e97d6615345bd7153ef3bac4c98187566
Anna <annaporay17@gmail.com>
2026-03-15 20:52:33 +0100

    new branch to try code

--- menu.py
+++ menu.py
@@ -3,7 +3,6 @@ from funkcje import show_workout_schedule, show_membership, show_cennik
 
 
 print("SIŁOWNIA ZASPANI")
-<<<<<<< Updated upstream
 menu_options = {
     1 : {
         "name": "Zajęcia",
@@ -72,21 +71,3 @@ while True:
         subvalue["function"]()
 
 print("Kończymy na dziś")
-=======
-# TODO opracowanie graficzne menu
-print("cennik")
-# TODO opracować cennik - print zawartość cennika, opracować graficznie
-print("kontakt")
-# TODO dodać informacje kontaktowe - adres, tel., e-mail, mapa
-# TODO e-mail - odnośnik otwierający nową wiadomość, tel. odnośnik umożliwiający wykonanie połączenia, mapa - odniesienie do google maps
-print("login")
-# TODO wprowadzić możliwość logowania się na konta: użytkownika siłowni, pracownika, admina? Stworzenie bazy klientów - class ludzie -
-# stworzenie kont pracowniczych - grafik pracy
-print("grafik")
-# TODO stworzyć tabelę z terminami dostępności siłowni - możliwość dla użytkownika zabookowania terminu?
-print("sklep")
-# TODO stworzyć koszyk z możliwością zakupów: zabookowania terminu z uwzględnieniem kart sportowych, z płatnością online oraz gotówkową.
-# Zakup różności innych.
-print("FAQ")
-# TODO stworzyć listę zapytań i odpowiedzi
->>>>>>> Stashed changes
