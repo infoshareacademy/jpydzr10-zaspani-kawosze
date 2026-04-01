687173ae349a88f27b3f8ad70889d16aef2f1747
Anna <annaporay17@gmail.com>
2026-04-01 23:41:25 +0200

    tabele z implementacja csv

--- grafik_i_cennik_kawosze.py
+++ grafik_i_cennik_kawosze.py
@@ -1,12 +1,11 @@
-import csv
-with open("grafik _zaspani_k.csv", mode='r') as file:
-    csv_reader = csv.reader(file)
-    next(csv_reader, None)
-    for row in csv_reader:
-        print(row)
 
-with open("cennik _zaspani_k.csv", mode='r') as file:
-    csv_reader = csv.reader(file)
-    next(csv_reader, None)
-    for row in csv_reader:
-        print(row)
+from prettytable import from_csv
+
+with open("cennik _zaspani_k.csv") as file:
+    table = from_csv(file)
+    print(table)
+
+with open("grafik _zaspani_k.csv") as file:
+    table = from_csv(file)
+    print(table)
+
