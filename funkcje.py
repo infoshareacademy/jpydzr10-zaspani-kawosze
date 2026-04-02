7a2fedf5e0d8f1ffe59a9f51f4dfbaca1b18370d
Anna <annaporay17@gmail.com>
2026-04-02 22:48:59 +0200

    próba działania

--- funkcje.py
+++ funkcje.py
@@ -1,19 +1,13 @@
-from cennik import get_membership_table_text
-from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parent
-SCHEDULE_FILE = PROJECT_ROOT / "grafik.csv"
+from prettytable import from_csv
 
+def kawosze_cennik():
+    with open("cennik _zaspani_k.csv") as file:
+        table = from_csv(file)
+        return table
 
-def show_workout_schedule():
-    if not SCHEDULE_FILE.exists():
-        print("Grafik nie jest jeszcze dostępny.")
-        return
+def kawosze_grafik():
+    with open("grafik _zaspani_k.csv") as file:
+        table = from_csv(file)
+        return table
 
-    with SCHEDULE_FILE.open(mode="r", encoding="utf-8") as file:
-        content = file.read()
-        print(content)
-
-
-def show_membership():
-    print(get_membership_table_text())
--- menu.py
+++ menu.py
@@ -1,24 +1,24 @@
-from funkcje import show_workout_schedule, show_membership
 
+from funkcje import kawosze_cennik, kawosze_grafik
 
 
 MENU_OPTIONS = {
     1: {
         "name": "Zajęcia",
-        "function": show_workout_schedule,
+        "function": kawosze_grafik() ,
     },
     2: {
         1: {
             "name": "Karnety",
-            "function": show_membership,
+            "function": kawosze_cennik(),
         },
         2: {
             "name": "Pakiety dla firm",
-            "function": show_workout_schedule,
+            "function": kawosze_grafik(),
         },
         3: {
             "name": "Tutaj coś jeszcze dodać można",
-            "function": show_workout_schedule,
+            "function": kawosze_grafik(),
         },
         4: {
             "name": "Powrót",
@@ -26,19 +26,19 @@ MENU_OPTIONS = {
     },
     3: {
         "name": "FAQ",
-        "function": show_workout_schedule,
+        "function": kawosze_grafik(),
     },
     4: {
         "name": "Kontakt",
-        "function": show_workout_schedule,
+        "function": kawosze_grafik(),
     },
     5: {
         "name": "Grafik",
-        "function": show_workout_schedule,
+        "function": kawosze_grafik(),
     },
     6: {
         "name": "Loguje się",
-        "function": show_workout_schedule,
+        "function": kawosze_grafik(),
     },
     7: {
         "name": "Wyjście",
