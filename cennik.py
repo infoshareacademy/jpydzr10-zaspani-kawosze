cbcb4b35895207a7faa3ffe616028cb3343c42fd
Anna <annaporay17@gmail.com>
2026-04-02 11:29:54 +0200

    cennik,grafik update

--- cennik.py
+++ cennik.py
@@ -4,10 +4,10 @@ from prettytable import PrettyTable
 def build_membership_table():
     table = PrettyTable()
     table.field_names = ["rodzaj wejścia", "ilość wejść", "cena", "sposób płatności"]
-    table.add_row(["karnet", "4 wejścia/mies", "6 kaw", "gotówka"])
-    table.add_row(["", "8 wejść/mies", "10 kaw", "karta/blik"])
-    table.add_row(["", "open", "12 kaw", "karta podarunkowa"])
-    table.add_row(["wejście pojedyńcze", "", "2 kawy", "karta typu Multisport"])
+    table.add_row(["karnet", "4 wejścia/mies", "50", "gotówka"])
+    table.add_row(["", "8 wejść/mies", "100", "karta/blik"])
+    table.add_row(["", "open", "120", "karta podarunkowa"])
+    table.add_row(["wejście pojedyńcze", "", "20", "karta typu Multisport"])
     return table
 
 
--- grafik_i_cennik_kawosze.py
+++ grafik_i_cennik_kawosze.py
@@ -1,11 +1,13 @@
 
 from prettytable import from_csv
 
-with open("cennik _zaspani_k.csv") as file:
-    table = from_csv(file)
-    print(table)
+def kawosze_cennik():
+    with open("cennik _zaspani_k.csv") as file:
+        table = from_csv(file)
+        return table
 
-with open("grafik _zaspani_k.csv") as file:
-    table = from_csv(file)
-    print(table)
+def kawosze_grafik():
+    with open("grafik _zaspani_k.csv") as file:
+        table = from_csv(file)
+        return table
 
